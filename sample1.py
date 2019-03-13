c5500432cae005dd7369f901ca6dca745f7e3e18
KensukeHirano <47747701+KensukeHirano@users.noreply.github.com>
2019-03-13 20:12:29 +0900

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -23,7 +23,7 @@ while True:
     if data is not None:
 
         if data.tick_last_update > latest_update:
-            #print('Illumination: {} lx'.format(data.val_light))
+            print('Illumination: {} lx'.format(data.val_light))
             #print(vars(data))
             print(data.val_temp)
 
