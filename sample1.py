e6667b7474d3b8588eaa3439d5bdf9e2ffc575a1
KensukeHirano <47747701+KensukeHirano@users.noreply.github.com>
2019-03-13 20:09:34 +0900

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -24,8 +24,7 @@ while True:
 
         if data.tick_last_update > latest_update:
             #print('Illumination: {} lx'.format(data.val_light))
-            #print(vars(data))
-            vars(data)
+            print(vars(data))
 
         latest_update = data.tick_last_update
 
